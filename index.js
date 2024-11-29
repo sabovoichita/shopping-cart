e9516070e5a1040b40d1e38f2151634905df14a1
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-29 17:21:17 +0100

    5. Next is to get your totals. Get the #total-items, #subtotal, #taxes, and #total elements. Store them in variables named totalNumberOfItems, cartSubTotal, cartTaxes, and cartTotal, respectively.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const cartContainer = document.getElementById("cart-container");
 const productsContainer = document.getElementById("products-container");
 const dessertCards = document.getElementById("dessert-card-container");
+const totalNumberOfItems = document.getElementById("total-items");
+const cartSubTotal = document.getElementById("subtotal");
+const cartTaxes = document.getElementById("taxes");
+const cartTotal = document.getElementById("total");
 
 console.log("Hey!");
