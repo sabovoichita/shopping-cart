96c52585daefa518be2a884b96651b88c8c8d8d9
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-29 19:44:15 +0100

    18. The this keyword in JavaScript is used to refer to the current object. Depending on where this is used, what it references changes. In the case of a class, it refers to the instance of the object being constructed. You can use the this keyword to set the properties of the object being instantiated. Here is an example:
    Example Code
    class Computer {
      constructor() {
        this.ram = 16;
      }
    }
    In your constructor, use the this keyword to set the items property to an empty array. Also, set the total property to 0, and the taxRate property to 8.25.

--- index.js
+++ index.js
@@ -98,5 +98,9 @@ products.forEach(({ name, id, price, category }) => {
 });
 
 class ShoppingCart {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.total = 0;
+    this.taxRate = 8.25;
+  }
 }
