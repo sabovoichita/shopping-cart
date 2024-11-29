5d2dab776b09b05d00b809c24eca6976d938304b
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-29 17:23:57 +0100

    6. The last element to get is the #show-hide-cart element. Store it in a variable named showHideCartSpan. Then, use let to declare a variable named isCartShowing and set it to false.

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ const totalNumberOfItems = document.getElementById("total-items");
 const cartSubTotal = document.getElementById("subtotal");
 const cartTaxes = document.getElementById("taxes");
 const cartTotal = document.getElementById("total");
+const showHideCartSpan = document.getElementById("show-hide-cart");
+let isCartShowing = false;
 
 console.log("Hey!");
