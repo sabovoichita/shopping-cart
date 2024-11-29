65167bb992d3ad5ca8a22306c029608ce4f8eca1
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-29 20:25:22 +0100

    26. You now have a small bug. When you try to access a property of an object and the property doesn't exist, you get undefined. This means if the dessert isn't already present in the totalCountPerProduct object, you end up trying to add 1 to undefined, which results in NaN. To fix this, you can use the || operator to set the value to 0 if it doesn't exist. Wrap your right-hand totalCountPerProduct[dessert.id] in parentheses, and add || 0 to the end of the expression.

--- index.js
+++ index.js
@@ -111,8 +111,8 @@ class ShoppingCart {
 
     const totalCountPerProduct = {};
     this.items.forEach((dessert) => {
-      totalCountPerProduct[dessert.id] = 0;
-      totalCountPerProduct[dessert.id] = totalCountPerProduct[dessert.id] + 1;
+      totalCountPerProduct[dessert.id] =
+        (totalCountPerProduct[dessert.id] || 0) + 1;
     });
   }
 }
