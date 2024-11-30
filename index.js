0fd41de4c7681c1ea800d0c3f881a437a989c93e
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-30 10:28:53 +0100

    33. In your first p element, add a span element. Give the span a class of product-count and an id of product-count-for-id${id}. Then, after the span, give your p element the text of the name variable.

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ class ShoppingCart {
       ? (currentProductCountSpan.textContent = `${currentProductCount}x`)
       : (productsContainer.innerHTML += `
         <div class="product" id="dessert${id}">
-            <p></p>
+            <p><span class="product-count" id="product-count-for-id${id}">${name}</span></p>
             <p>${price}</p>
         </div>`);
   }
